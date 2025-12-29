bcc0302dc347b7ac0ae0af1f73373be2002f0f0e
u5-kc-dev <u5-kc-vm@email.com>
2025-12-29 18:06:38 +0530

    initial codebase

---
+++ update-system.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "updating local repos"
+sudo apt-get update -y
+
+echo "upgrading installed packages"
+sudo apt-get upgrade -y
+
+echo "removing deprecated packages"
+sudo apt-get autoremove
