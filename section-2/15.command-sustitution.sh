e85819d758fa7097c6aac887856f5a30d52324a1
u5-kc-dev <u5-kc-vm@email.com>
2026-01-19 17:25:46 +0530

    +

---
+++ section-2/15.command-sustitution.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time=$(date +%H:%M:%S)
+echo "hi there $USER, the current time is $time"
---
+++ section-2/15.sh
@@ -0,0 +1,26 @@
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-2$ 
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-2$ 
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-2$ PS1='\$ "
+> ^C
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-2$ PS1='\$ '
+$ date
+Mon Jan 19 05:06:18 PM IST 2026
+$ chmod 744 15.command-sustitution.sh 
+$ ./15.command-sustitution.sh 
+$ ./15.command-sustitution.sh 
+time=Mon Jan 19 05:08:58 PM IST 2026
+$ ./15.command-sustitution.sh 
+time=Mon Jan 19 05:09:36 PM IST 2026
+
+$ ./15.command-sustitution.sh 
+Mon Jan 19 05:10:01 PM IST 2026
+$ ./15.command-sustitution.sh 
+hi there user1, the current time is 17:15:28
+$ pwd
+/home/user1/Desktop/U5-bash-scripts-practice/section-2
+$ echo $PWD
+/home/user1/Desktop/U5-bash-scripts-practice/section-2
+$ PWD=dshfdfhdfhdfsgh
+$ echo $PWD
+dshfdfhdfhdfsgh
+$ 
