1a8e9c83c9eefbd0192f01412c0cd96614da99ed
u5-kc-dev <u5-kc-vm@email.com>
2026-02-20 18:58:59 +0530

    +

---
+++ section-4/49.select/0.sh
@@ -0,0 +1,47 @@
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ 
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ chmod 755 script.sh 
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ ./script.sh 
+1) monday
+2) tueday
+3) wednesday
+4) thursday
+5) friday
+6) saturday
+7) sunday
+#? 4
+the chosen day of the wee is thursday
+#? 6
+the chosen day of the wee is saturday
+#? ^C
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ bash script1.sh 
+1) monday
+2) tueday
+3) wednesday
+4) thursday
+5) friday
+6) saturday
+7) sunday
+#? 7
+the chosen day of the week is: sunday
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ bash script2.sh 
+1) monday
+2) tueday
+3) wednesday
+4) thursday
+5) friday
+6) saturday
+7) sunday
+choose the required day of the week: 1
+the chosen day of the week is: monday
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ bash script2.sh 
+1) monday
+2) tueday
+3) wednesday
+4) thursday
+5) friday
+6) saturday
+7) sunday
+choose the required day of the week: asdfasd
+the chosen day of the week is: 
+user1@user1-VM:~/Desktop/U5-bash-scripts-practice/section-4/49.select$ 
+
